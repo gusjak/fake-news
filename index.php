c387eb2addd27a58c8d658a2d21b2cbf60e7928c
gusjak <70512255+gusjak@users.noreply.github.com>
2020-10-23 15:52:52 +0200

    Styling to website
    Added basic stryling and look to the header and the article cards.

--- index.php
+++ index.php
@@ -4,10 +4,11 @@
 // keep us much logic out of the HTML as possible. Put the PHP logic in the top
 // of the files containing HTML or even better; in another PHP file altogether.
 
-require __DIR__ . '/data.php';
-require __DIR__ . '/functions.php';
+// require __DIR__ . '/data.php';
+// require __DIR__ . '/functions.php';
 
 ?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -15,9 +16,49 @@ require __DIR__ . '/functions.php';
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Fake News</title>
+    <link rel="stylesheet" href="/global.css">
+    <link rel="stylesheet" href="/typography.css">
+    <link rel="stylesheet" href="/header.css">
+    <link rel="stylesheet" href="/main.css">
 </head>
 
+<header>
+    <h1 data-content="Fake">News</h1>
+</header>
+
 <body>
+    <main>
+        <article>
+            <h2>Title 1</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias ea repellendus omnis rerum nemo, eos consectetur esse nihil quod non deleniti minus debitis necessitatibus aspernatur suscipit ipsa optio. Maiores minima dolorem amet quos veniam eius dolore minus quas nam vero.</p>
+            <span>Author 1 - 20/10-2010 - 3782 Likes</span>
+        </article>
+        <article>
+            <h2>Title 2</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias ea repellendus omnis rerum nemo, eos consectetur esse nihil quod non deleniti minus debitis necessitatibus aspernatur suscipit ipsa optio. Maiores minima dolorem amet quos veniam eius dolore minus quas nam vero.</p>
+            <span>Author 2 - 20/10-2010 - 378 Likes</span>
+        </article>
+        <article>
+            <h2>Title 1</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias ea repellendus omnis rerum nemo, eos consectetur esse nihil quod non deleniti minus debitis necessitatibus aspernatur suscipit ipsa optio. Maiores minima dolorem amet quos veniam eius dolore minus quas nam vero.</p>
+            <span>Author 1 - 20/10-2010 - 3782 Likes</span>
+        </article>
+        <article>
+            <h2>Title 2</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias ea repellendus omnis rerum nemo, eos consectetur esse nihil quod non deleniti minus debitis necessitatibus aspernatur suscipit ipsa optio. Maiores minima dolorem amet quos veniam eius dolore minus quas nam vero.</p>
+            <span>Author 2 - 20/10-2010 - 378 Likes</span>
+        </article>
+        <article>
+            <h2>Title 1</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias ea repellendus omnis rerum nemo, eos consectetur esse nihil quod non deleniti minus debitis necessitatibus aspernatur suscipit ipsa optio. Maiores minima dolorem amet quos veniam eius dolore minus quas nam vero.</p>
+            <span>Author 1 - 20/10-2010 - 3782 Likes</span>
+        </article>
+        <article>
+            <h2>Title 2</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias ea repellendus omnis rerum nemo, eos consectetur esse nihil quod non deleniti minus debitis necessitatibus aspernatur suscipit ipsa optio. Maiores minima dolorem amet quos veniam eius dolore minus quas nam vero.</p>
+            <span>Author 2 - 20/10-2010 - 378 Likes</span>
+        </article>
+    </main>
 </body>
 
 </html>
