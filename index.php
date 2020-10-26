2edfc80bbad7f974a6159276900af349151ff8c2
gusjak <70512255+gusjak@users.noreply.github.com>
2020-10-26 09:56:39 +0100

    Cleanup
    Cleaned up unnecessary code and added styling to author title.

--- index.php
+++ index.php
@@ -31,10 +31,10 @@ require __DIR__ . '/functions.php';
     <main>
         <?php usort($newsItems, 'sortByDate'); ?>
         <?php foreach ($newsItems as $newsItem) : ?>
-            <article class="item-1">
+            <article class="item">
                 <img src="<?php echo $newsItem['image'] ?>" class="responsive" alt="article image">
                 <ul>
-                    <li><?php echo $newsItem['author'] ?></li>
+                    <li><a class="link-2" href="#"><?php echo $newsItem['author'] ?></a></li>
                     <li><?php echo date("d/m-Y", $newsItem['datePublished']) ?></li>
                 </ul>
                 <h2><?php echo $newsItem['title'] ?></h2>
@@ -42,99 +42,6 @@ require __DIR__ . '/functions.php';
                 <span><?php echo $newsItem['likes'] ?> <a class="link-1" href=""> Likes</a></span>
             </article>
         <?php endforeach; ?>
-
-        <!-- 
-            <article class="item">
-            <div class="thumbnail" style="background-image: url(https://images.unsplash.com/photo-1572657018299-fcb6280c9356?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1629&q=80)"></div>
-            <ul>
-                <li>Author</li>
-                <li>20/10-2010</li>
-            </ul>
-            <h2>Title 2</h2>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias ea repellendus omnis rerum nemo, eos consectetur esse nihil quod non deleniti minus debitis necessitatibus aspernatur suscipit ipsa optio. Maiores minima dolorem amet quos veniam eius dolore minus quas nam vero.</p>
-            <span>3782 Likes</span>
-        </article>
-        <article class="item">
-            <div class="thumbnail" style="background-image: url(https://images.unsplash.com/photo-1572657018299-fcb6280c9356?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1629&q=80)"></div>
-            <ul>
-                <li>Author</li>
-                <li>20/10-2010</li>
-            </ul>
-            <h2>Title 2</h2>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias ea repellendus omnis rerum nemo, eos consectetur esse nihil quod non deleniti minus debitis necessitatibus aspernatur suscipit ipsa optio. Maiores minima dolorem amet quos veniam eius dolore minus quas nam vero.</p>
-            <span>3782 Likes</span>
-        </article>
-        <article class="item">
-            <div class="thumbnail" style="background-image: url(https://images.unsplash.com/photo-1572657018299-fcb6280c9356?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1629&q=80)"></div>
-            <ul>
-                <li>Author</li>
-                <li>20/10-2010</li>
-            </ul>
-            <h2>Title 2</h2>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias ea repellendus omnis rerum nemo, eos consectetur esse nihil quod non deleniti minus debitis necessitatibus aspernatur suscipit ipsa optio. Maiores minima dolorem amet quos veniam eius dolore minus quas nam vero.</p>
-            <span>3782 Likes</span>
-        </article>
-        <article class="item">
-            <div class="thumbnail" style="background-image: url(https://images.unsplash.com/photo-1572657018299-fcb6280c9356?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1629&q=80)"></div>
-            <ul>
-                <li>Author</li>
-                <li>20/10-2010</li>
-            </ul>
-            <h2>Title 2</h2>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias ea repellendus omnis rerum nemo, eos consectetur esse nihil quod non deleniti minus debitis necessitatibus aspernatur suscipit ipsa optio. Maiores minima dolorem amet quos veniam eius dolore minus quas nam vero.</p>
-            <span>3782 Likes</span>
-        </article>
-        <article class="item">
-            <div class="thumbnail" style="background-image: url(https://images.unsplash.com/photo-1572657018299-fcb6280c9356?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1629&q=80)"></div>
-            <ul>
-                <li>Author</li>
-                <li>20/10-2010</li>
-            </ul>
-            <h2>Title 2</h2>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias ea repellendus omnis rerum nemo, eos consectetur esse nihil quod non deleniti minus debitis necessitatibus aspernatur suscipit ipsa optio. Maiores minima dolorem amet quos veniam eius dolore minus quas nam vero.</p>
-            <span>3782 Likes</span>
-        </article>
-        <article class="item">
-            <div class="thumbnail" style="background-image: url(https://images.unsplash.com/photo-1572657018299-fcb6280c9356?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1629&q=80)"></div>
-            <ul>
-                <li>Author</li>
-                <li>20/10-2010</li>
-            </ul>
-            <h2>Title 2</h2>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias ea repellendus omnis rerum nemo, eos consectetur esse nihil quod non deleniti minus debitis necessitatibus aspernatur suscipit ipsa optio. Maiores minima dolorem amet quos veniam eius dolore minus quas nam vero.</p>
-            <span>3782 Likes</span>
-        </article>
-        <article class="item">
-            <div class="thumbnail" style="background-image: url(https://images.unsplash.com/photo-1572657018299-fcb6280c9356?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1629&q=80)"></div>
-            <ul>
-                <li>Author</li>
-                <li>20/10-2010</li>
-            </ul>
-            <h2>Title 2</h2>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias ea repellendus omnis rerum nemo, eos consectetur esse nihil quod non deleniti minus debitis necessitatibus aspernatur suscipit ipsa optio. Maiores minima dolorem amet quos veniam eius dolore minus quas nam vero.</p>
-            <span>3782 Likes</span>
-        </article>
-        <article class="item">
-            <div class="thumbnail" style="background-image: url(https://images.unsplash.com/photo-1572657018299-fcb6280c9356?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1629&q=80)"></div>
-            <ul>
-                <li>Author</li>
-                <li>20/10-2010</li>
-            </ul>
-            <h2>Title 2</h2>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias ea repellendus omnis rerum nemo, eos consectetur esse nihil quod non deleniti minus debitis necessitatibus aspernatur suscipit ipsa optio. Maiores minima dolorem amet quos veniam eius dolore minus quas nam vero.</p>
-            <span>3782 Likes</span>
-        </article>
-        <article class="item">
-            <div class="thumbnail" style="background-image: url(https://images.unsplash.com/photo-1572657018299-fcb6280c9356?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1629&q=80)"></div>
-            <ul>
-                <li>Author</li>
-                <li>20/10-2010</li>
-            </ul>
-            <h2>Title 2</h2>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias ea repellendus omnis rerum nemo, eos consectetur esse nihil quod non deleniti minus debitis necessitatibus aspernatur suscipit ipsa optio. Maiores minima dolorem amet quos veniam eius dolore minus quas nam vero.</p>
-            <span>3782 Likes</span>
-        </article>
-        -->
     </main>
 </body>
 
